dcb6eff313481a18285552d3a00cd8fcbf6fea49
panicdragon <d.takayama.web@gmail.com>
2024-09-27 02:00:55 +0900

    feat: 🎸 init

---
+++ Formula/open-texture-packer.rb
@@ -0,0 +1,15 @@
+class OpenTexturePacker < Formula
+  desc "Open source and free Texture Packer tool written in rust."
+  homepage "https://github.com/webcyou-org/open-texture-packer"
+  url "https://github.com/webcyou-org/open-texture-packer/releases/download/v0.1.7/otp-v0.1.7-x86_64-apple-darwin.tar.gz"
+  version "0.1.7"
+  sha256 "878d9fb3b5aaae00cc8d13abe49e18811e8c643fa1c921900a7c93277a1c79c2"
+
+  def install
+    bin.install "otp"
+  end
+
+  test do
+    system "#{bin}/otp --version"
+  end
+end
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # homebrew-tap
+
+Homebrew formulae that allows installation of webcyou tools through the Homebrew package manager.
+
+
+## Installation
+
+```
+brew tap webcyou-org/tap
+brew install <FORMULA>
+```
+
+## Formulae
+
+| Repository              |   Formula   | Description |
+|:------------------------|:-----------:|:-----------:|
+| [open-texture-packer](https://github.com/webcyou-org/open-texture-packer) | formula  | Open source and free Texture Packer tool written in rust. |
