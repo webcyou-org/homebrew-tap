72597a649dc0366b4e28390cd869a3e8f543560d
panicdragon <d.takayama.web@gmail.com>
2024-09-28 02:16:16 +0900

    feat: 🎸 update open-texture-packer v0.2.0

--- Formula/open-texture-packer.rb
+++ Formula/open-texture-packer.rb
@@ -1,9 +1,9 @@
 class OpenTexturePacker < Formula
   desc "Open source and free Texture Packer tool written in rust."
   homepage "https://github.com/webcyou-org/open-texture-packer"
-  url "https://github.com/webcyou-org/open-texture-packer/releases/download/v0.1.7/otp-v0.1.7-x86_64-apple-darwin.tar.gz"
-  version "0.1.7"
-  sha256 "878d9fb3b5aaae00cc8d13abe49e18811e8c643fa1c921900a7c93277a1c79c2"
+  url "https://github.com/webcyou-org/open-texture-packer/releases/download/v0.2.0/otp-v0.2.0-x86_64-apple-darwin.tar.gz"
+  version "0.2.0"
+  sha256 "f36e8a398d9c98b1129e89a679e56aedf7d1f2aa3d19bb6b34a595686cea81d1"
   license "MIT"
 
   def install
