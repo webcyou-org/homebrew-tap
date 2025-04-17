3d8a9bbce318f367e0941f54974b3ed68d023e1d
panicdragon <d.takayama.web@gmail.com>
2025-04-17 11:03:00 +0900

    fix: 🐛 update sha256

--- Formula/typeflow.rb
+++ Formula/typeflow.rb
@@ -3,7 +3,7 @@ class Typeflow < Formula
   homepage "https://github.com/webcyou-org/typeflow"
   url "https://github.com/webcyou-org/typeflow/releases/download/v0.1.0/typeflow-v0.1.0-x86_64-apple-darwin.tar.gz"
   version "0.1.0"
-  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
+  sha256 "5d80bc5a8653d2077fc80ebd36920092a160402a3be0fa1c79c39b987477997a"
   license "MIT"
 
   def install
