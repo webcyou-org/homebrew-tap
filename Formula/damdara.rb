c6a7c229d335878b07c6febc96e7e52abb01efb4
panicdragon <d.takayama.web@gmail.com>
2025-04-29 13:44:56 +0900

    refactor: 💡 update sha256

--- Formula/damdara.rb
+++ Formula/damdara.rb
@@ -3,7 +3,7 @@ class Damdara < Formula
   homepage "https://github.com/retrodig/damdara"
   url "https://github.com/retrodig/damdara/releases/download/v0.8.0/damdara-v0.8.0-x86_64-apple-darwin.tar.gz"
   version "0.8.0"
-  sha256 "3a5fcbbc2cd59b99de1477ca11cc17251ff5c99b427678c4b935371c870b62dd"
+  sha256 "ef8ca471ea046ea01f10d0137452b681d0ffd6564a45d773abb9d7711bc2fa17"
   license "MIT"
 
   def install
