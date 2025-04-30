adfeaa98d6083039f0ab00e916d26ba6b58ed128
panicdragon <d.takayama.web@gmail.com>
2025-05-01 01:24:44 +0900

    feat: 🎸 damdara v0.8.1

--- Formula/damdara.rb
+++ Formula/damdara.rb
@@ -1,9 +1,9 @@
 class Damdara < Formula
   desc "Dragon Quest Core Module, created in Rust."
   homepage "https://github.com/retrodig/damdara"
-  url "https://github.com/retrodig/damdara/releases/download/v0.8.0/damdara-v0.8.0-x86_64-apple-darwin.tar.gz"
-  version "0.8.0"
-  sha256 "ef8ca471ea046ea01f10d0137452b681d0ffd6564a45d773abb9d7711bc2fa17"
+  url "https://github.com/retrodig/damdara/releases/download/v0.8.1/damdara-v0.8.1-x86_64-apple-darwin.tar.gz"
+  version "0.8.1"
+  sha256 "57fc26c5b3ee5f9c023fc83a30b79791c853c564b18c56772d856e878e047bf1"
   license "MIT"
 
   def install
